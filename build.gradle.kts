352485d49d273fd9014ffe5da9d94d7c04bc6cf2
macCali <jungcali94@gmail.com>
2023-01-27 21:35:09 +0900

    #4 프로젝트 설정
    - 의존성 추가
    - application.yml h2 db 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,21 +2,86 @@ plugins {
 	java
 	id("org.springframework.boot") version "2.7.7"
 	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	id("org.asciidoctor.jvm.convert") version "3.3.2"
 }
 
-group = "com"
+group = "com.flab"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
 
 repositories {
 	mavenCentral()
 }
 
+extra["snippetsDir"] = file("build/generated-snippets")
+
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.h2database:h2")
+	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.restdocs:spring-restdocs-asciidoctor")
+	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+tasks {
+	withType<Test> {
+		useJUnitPlatform()
+	}
+
+	val snippetsDir = file("$buildDir/generated-snippets")
+
+	clean {
+		delete("src/main/resources/static/docs")
+	}
+
+	test {
+		useJUnitPlatform()
+		systemProperty("org.springframework.restdocs.outputDir", snippetsDir)
+		outputs.dir(snippetsDir)
+	}
+
+	build {
+		dependsOn("copyDocument")
+	}
+
+	asciidoctor {
+		dependsOn(test)
+
+		attributes(
+				mapOf("snippets" to snippetsDir)
+		)
+		inputs.dir(snippetsDir)
+
+		doFirst {
+			delete("src/main/resources/static/docs")
+		}
+	}
+
+	register<Copy>("copyDocument") {
+		dependsOn(asciidoctor)
+
+		destinationDir = file(".")
+		from(asciidoctor.get().outputDir) {
+			into("src/main/resources/static/docs")
+		}
+	}
+
+	bootJar {
+		dependsOn(asciidoctor)
+
+		from(asciidoctor.get().outputDir) {
+			into("BOOT-INF/classes/static/docs")
+		}
+	}
 }
--- src/main/java/com/flab/modu/ModuUiMarketApplication.java
+++ src/main/java/com/flab/modu/ModuUiMarketApplication.java
@@ -1,4 +1,4 @@
-package com.modu;
+package com.flab.modu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
