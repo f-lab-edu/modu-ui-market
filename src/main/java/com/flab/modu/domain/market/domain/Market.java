a1b0f6d3c6909805d6665665dfcf827e15c0b9ab
yujin-cha <ckdbwls11@naver.com>
2023-02-03 00:16:01 +0900

    #7 - 마켓 도메인 엔티티 및 JPA레파지토리 추가
    * 마켓 엔티티 추가
       - DB테이블과 컬럼 매핑되도록 설정
       - Getter와 ToString이 자동생성되도록 설정
       - Date정보가 자동으로 생성되어 DB에 들어갈 수 있도록 @EntityListeners 추가. 이와 관련하여 JPAConfig클래스에 @EnableJpaAuditing도 추가
    
    * JPA레파지토리 추가
       - 마켓 도메인의 DB 처리시 사용할 JPA레파지토리 추가

---
+++ src/main/java/com/flab/modu/domain/market/domain/Market.java
@@ -0,0 +1,91 @@
+package com.flab.modu.domain.market.domain;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class Market {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Setter
+  @Column(nullable = false, length = 50)
+  @NotNull
+  private String sellerId;
+
+  @Setter
+  @Column(nullable = false, length = 200)
+  @NotNull
+  private String name;
+
+  @Setter
+  @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
+  private MarketStatus status;
+
+  @Setter
+  @Column(nullable = false, length = 100)
+  @NotNull
+  private String url;
+
+  @CreatedDate
+  @Column(nullable = false)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  @Column(nullable = false)
+  private LocalDateTime modifiedAt;
+
+  public Market() {
+  }
+
+  public Market(String sellerId, String name, String url) {
+    this.sellerId = sellerId;
+    this.name = name;
+    this.url = url;
+  }
+
+  public static Market of(String sellerId, String name, String url) {
+    return new Market(sellerId, name, url);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Market)) {
+      return false;
+    }
+    Market market = (Market) o;
+    return id.equals(market.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+
+}
---
+++ src/main/java/com/flab/modu/domain/market/repository/MarketRepository.java
@@ -0,0 +1,8 @@
+package com.flab.modu.domain.market.repository;
+
+import com.flab.modu.domain.market.domain.Market;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MarketRepository extends JpaRepository<Market, Long> {
+
+}
