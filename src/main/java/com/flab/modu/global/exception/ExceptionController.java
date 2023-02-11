1de6db9d38ce9d12340bbec7ec9ba7c4a2661f41
macCali <jungcali94@gmail.com>
2023-02-11 14:14:03 +0900

    예외처리 컨트롤러 추가
    # ExceptionController
        - RestControllerAdvice & ExceptionHandler를 이용해 컨트롤러 단의 Exception을 공통으로 처리

---
+++ src/main/java/com/flab/modu/global/exception/ExceptionController.java
@@ -0,0 +1,21 @@
+package com.flab.modu.global.exception;
+
+import com.flab.modu.users.exception.DuplicatedEmailException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(DuplicatedEmailException.class)
+    public final ResponseEntity<String> handleDuplicatedEmailException(
+        DuplicatedEmailException exception) {
+        log.debug("중복된 이메일입니다.", exception);
+
+        return new ResponseEntity<>("이미 가입된 이메일입니다.", HttpStatus.CONFLICT);
+    }
+}
