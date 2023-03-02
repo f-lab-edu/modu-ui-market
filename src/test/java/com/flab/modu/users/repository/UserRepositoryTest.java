33bca747fd25ca1def73090cffb51d58fab581d8
yujin-cha <ckdbwls11@naver.com>
2023-03-02 16:20:51 +0900

    프로파일 - 프로파일 기본값 세팅, DB정보가 필요한 TEST코드에 @ActiveProfiles추가
    - 프로파일 옵션이 들어오지 않은 경우 동작할 프로파일 기본값을 local로 설정함
    - DB접근이 필요한 즉, test프로파일의 DB정보가 필요한 테스트 코드에 @ActiveProfiles("test")를 추가함

--- src/test/java/com/flab/modu/users/repository/UserRepositoryTest.java
+++ src/test/java/com/flab/modu/users/repository/UserRepositoryTest.java
@@ -12,8 +12,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @DisplayName("JPA 연결 테스트")
+@ActiveProfiles("test")
 @Import(JpaConfig.class)
 @DataJpaTest
 class UserRepositoryTest {
