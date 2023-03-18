4ab441dccface8ec8cda95940a461b3a5442316e
yujin-cha <ckdbwls11@naver.com>
2023-03-19 00:39:19 +0900

    #6 - Configurer에 ProductStatusCodeRequestConverter 등록하는 코드 추가
    상품 등록 API에서 파라미터로 넘어오는 ProductStatusCode를 enum값으로 변환하는 작업이 필요하다. 이를 위해 ProductStatusCodeRequestConverter를 WebMvcConfigurer에 등록함

--- src/main/java/com/flab/modu/global/config/WebConfig.java
+++ src/main/java/com/flab/modu/global/config/WebConfig.java
@@ -1,8 +1,11 @@
 package com.flab.modu.global.config;
 
 import com.flab.modu.global.interceptor.LoginCheckInterceptor;
+import com.flab.modu.product.converter.ProductStatusCodeRequestConverter;
+import com.flab.modu.product.domain.common.ProductStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,8 +15,15 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final LoginCheckInterceptor loginCheckInterceptor;
 
+    private final ProductStatusCodeRequestConverter productStatusCodeRequestConverter;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginCheckInterceptor);
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(productStatusCodeRequestConverter);
     }
 }
