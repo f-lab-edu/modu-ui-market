a28806c8117f35259946aa0f237ffbe10a352e1a
macCali <jungcali94@gmail.com>
2023-04-06 21:11:22 +0900

    로그인 유저 email을 가져오는 어노테이션 추가
    * LoginUserArgumentResolver

--- src/main/java/com/flab/modu/global/config/WebConfig.java
+++ src/main/java/com/flab/modu/global/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.flab.modu.global.config;
 
 import com.flab.modu.global.interceptor.LoginCheckInterceptor;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,8 +14,15 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final LoginCheckInterceptor loginCheckInterceptor;
 
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginCheckInterceptor);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
     }
 }
