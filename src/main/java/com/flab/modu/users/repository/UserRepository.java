174e7837e25967fac1a87657aa4372dd3a4fea8e
macCali <jungcali94@gmail.com>
2023-04-06 14:07:01 +0900

    회원탈퇴 Service 로직 구현
    *회원탈퇴 logic 구현
      - 회원탈퇴 서비스 테스트 작성

--- src/main/java/com/flab/modu/users/repository/UserRepository.java
+++ src/main/java/com/flab/modu/users/repository/UserRepository.java
@@ -9,4 +9,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     Optional<User> findByEmailAndPassword(String email, String password);
+
+    void deleteByEmail(String email);
 }
