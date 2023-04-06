8d112b8075cf61b39e1a204f5075ea795f8a43a2
macCali <jungcali94@gmail.com>
2023-04-06 21:13:02 +0900

    회원탈퇴 Controller 메소드 추가
    * 회원탈퇴 Controller 메소드 추가
      - Test 코드 추가

--- src/main/java/com/flab/modu/users/controller/UserController.java
+++ src/main/java/com/flab/modu/users/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.flab.modu.users.controller;
 
+import com.flab.modu.global.annotation.CurrentUser;
 import com.flab.modu.global.annotation.LoginCheck;
 import com.flab.modu.users.controller.UserDto.LoginRequest;
 import com.flab.modu.users.service.LoginService;
@@ -7,11 +8,11 @@ import com.flab.modu.users.service.UserService;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -23,23 +24,32 @@ public class UserController {
     private final LoginService loginService;
 
     @PostMapping("/users")
-    public ResponseEntity<Void> createUser(@RequestBody @Valid UserDto.CreateRequest createRequest) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createUser(@RequestBody @Valid UserDto.CreateRequest createRequest) {
         userService.createUser(createRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @PostMapping("users/login")
+    @LoginCheck
+    @DeleteMapping("/users")
+    public void userWithdrawal(@RequestBody @Valid UserDto.PasswordRequest requestDto,
+        @CurrentUser String email) {
+        String password = requestDto.getPassword();
+        userService.delete(email, password);
+        loginService.logout();
+    }
+
+    @PostMapping("/users/login")
     public void login(@RequestBody @Valid UserDto.LoginRequest loginRequest) {
         loginService.login(loginRequest);
     }
 
     @LoginCheck
-    @DeleteMapping("users/logout")
+    @DeleteMapping("/users/logout")
     public void logout() {
         loginService.logout();
     }
 
-    @GetMapping("users/session")
+    @GetMapping("/users/session")
     public String getUserInfo(LoginRequest loginRequest) {
         return loginService.getLoginUser();
     }
